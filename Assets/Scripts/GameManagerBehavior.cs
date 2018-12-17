3a3884cc99cc1428bd9748c15c6cf3a153816385
doan.dominh <doan.dominh@gameloft.com>
2018-12-17 12:56:26 +0700

    - adding Game Manager + handle gold value

---
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerBehavior : MonoBehaviour
+{
+    public Text goldLabel;
+
+    int gold;
+    public int Gold
+    {
+        get
+        {
+            return gold;
+        }
+        set
+        {
+            gold = value;
+            goldLabel.text = "GOLD: " + gold.ToString();
+        }
+    }   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Gold = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
