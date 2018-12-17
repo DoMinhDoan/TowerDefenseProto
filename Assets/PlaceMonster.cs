26a65d4a3df379fe92d12f93a803424ec92328b3
doan.dominh <doan.dominh@gameloft.com>
2018-12-17 11:12:11 +0700

    - added open spot and place monster inside

---
+++ Assets/PlaceMonster.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceMonster : MonoBehaviour
+{
+    public GameObject monsterPrefab;
+    GameObject monster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool CanReplaceMonster()
+    {
+        return monster == null;
+    }
+
+    void OnMouseUp()
+    {
+        if(CanReplaceMonster())
+        {
+            monster = Instantiate(monsterPrefab, this.transform.position, Quaternion.identity);
+
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+    }
+}
