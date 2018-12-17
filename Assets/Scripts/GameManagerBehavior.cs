98b3690aaebea08e754e44b587eced4c15c37c09
doan.dominh <doan.dominh@gameloft.com>
2018-12-17 15:06:38 +0700

    - wave information

--- Assets/Scripts/GameManagerBehavior.cs
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class GameManagerBehavior : MonoBehaviour
 {
     public Text goldLabel;
+    public Text waveLabel;
+    public GameObject[] nextWaveLabels;
+    public bool isGameOver = false;
 
     int gold;
     public int Gold
@@ -19,13 +22,37 @@ public class GameManagerBehavior : MonoBehaviour
             gold = value;
             goldLabel.text = "GOLD: " + gold.ToString();
         }
-    }   
+    }
+
+    int wave;
+    public int Wave
+    {
+        get
+        {
+            return wave;
+        }
+        set
+        {
+            wave = value;
+
+            if(!isGameOver)
+            {
+                foreach(var go in nextWaveLabels)
+                {
+                    go.GetComponent<Animator>().SetTrigger("nextWave");
+                }
+            }
+
+            goldLabel.text = "WAVE: " + (gold + 1).ToString();
+        }
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
         Gold = 1000;
+        Wave = 0;
     }
 
     // Update is called once per frame
