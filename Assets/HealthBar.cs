4c2d63c55b46af8645032ee94307a1b625c5a6d4
dodoan <dodoan.it@gmail.com>
2018-12-17 23:40:24 +0700

    + added health bar.

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public float maxHealth = 100;
+    public float currentHealth = 100;
+    float originalScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalScale = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 scale = gameObject.transform.localScale;
+        scale.x = originalScale * currentHealth / maxHealth;
+        gameObject.transform.localScale = scale;
+    }
+}
